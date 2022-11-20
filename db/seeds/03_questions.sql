c980b4e90f203dac42227c793ac75dd97371d5be
tatianibassani <tatianibassani@gmail.com>
2022-11-20 00:45:33 +0000

    Change the descriptions to Adventure in the seeds data

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -1,10 +1,10 @@
 -- Initial data in order to do some tests.
 
 -- Inserting questions to the poll 1 (Which movies)
-INSERT INTO questions (name, description, poll_id) VALUES ('Lord of the Rings', 'Description', 1);
-INSERT INTO questions (name, description, poll_id) VALUES ('Thor', 'Description', 1);
-INSERT INTO questions (name, description, poll_id) VALUES ('The avengers', 'Description', 1);
-INSERT INTO questions (name, description, poll_id) VALUES ('Iron Man 2', 'Description', 1);
+INSERT INTO questions (name, description, poll_id) VALUES ('Lord of the Rings', 'Adventure', 1);
+INSERT INTO questions (name, description, poll_id) VALUES ('Thor', 'Adventure', 1);
+INSERT INTO questions (name, description, poll_id) VALUES ('The avengers', 'Adventure', 1);
+INSERT INTO questions (name, description, poll_id) VALUES ('Iron Man 2', 'Adventure', 1);
 
 -- Inserting questions to the poll 2 (Place to next trip)
 INSERT INTO questions (name, description, poll_id) VALUES ('Paris', 'Description', 2);
