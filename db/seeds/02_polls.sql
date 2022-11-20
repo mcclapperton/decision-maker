7333a986842456ae29c7d1513da376c6d9708919
tatianibassani <tatianibassani@gmail.com>
2022-11-20 00:16:37 +0000

    Add adminstrativeLink and submissionLink columns to the polls table

--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -1,7 +1,7 @@
 -- Initial data in order to do some tests.
 
 -- Creating poll 1
-INSERT INTO polls (title, email) VALUES ('Which movie are we going to watch today?', 'creator1@test.com');
+INSERT INTO polls (title, email, adminstrativeLink, submissionLink) VALUES ('Which movie are we going to watch today?', 'creator1@test.com', '/polls/1', '/polls/results/1');
 
 -- Creating poll 2
-INSERT INTO polls (title, email) VALUES ('Where are we going in our next trip?', 'creator2@test.com');
+INSERT INTO polls (title, email, adminstrativeLink, submissionLink) VALUES ('Where are we going in our next trip?', 'creator2@test.com', '/polls/2', '/polls/results/2');
