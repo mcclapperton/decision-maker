45d6742e800d244c2519b27ce38658122c5b94ff
Maris <mcclapperton@gmail.com>
2022-11-22 21:47:41 -0700

    added addOption listener and createNewOption fucntion to append in index template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,33 @@
-// add new option
-// new poll
-// submit
-// drag and drop
+$(document).ready(function () {
+  // Preventing XSS with Escaping
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // add new option
+  const createNewOption = function () {
+    const $option = $(`
+    <div class="new-option">
+    <label for="option">Option:</label>
+      <input
+        type="text"
+        class="optionN"
+        placeholder="Enter an option"/>
+        </div>
+        `);
+    return $option;
+  };
+  // append createNewOption to the bottom of form when add option clicked
+  const addOption = function () {
+    $(".add-option").on("click", function (element) {
+      $("create-poll-form").append(createNewOption());
+    });
+  };
+  // new poll-->ejs?
+
+  // submit-->ejs?
+
+  // drag and drop
+});
